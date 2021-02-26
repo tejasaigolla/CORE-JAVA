78595fd03e46c6e17b9a1e90fbf2a3369845656e
tejasaigolla <78849267+tejasaigolla@users.noreply.github.com>
2021-02-26 16:55:03 +0530

    Create Employee.java

---
+++ day2/Employee.java
@@ -0,0 +1,30 @@
+class Employee
+{
+private String name;
+private String address;
+private String mobile;
+public String getname()
+{
+return name;
+}
+public String getaddress()
+{
+return address;
+}
+public String getmobileno()
+{
+return mobile;
+}
+public void setname(String name)
+{
+this.name=name;
+}
+public void setaddress(String addr)
+{
+address=addr;
+}
+public void setmobileno(String mobno)
+{
+mobile=mobno;
+}
+}
