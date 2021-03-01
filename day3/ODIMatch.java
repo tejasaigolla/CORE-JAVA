617e236b5022765a4dc8206d7fdb68d7a5d4f0cc
tejasaigolla <78849267+tejasaigolla@users.noreply.github.com>
2021-03-01 09:25:50 +0530

    Create ODIMatch.java

---
+++ day3/ODIMatch.java
@@ -0,0 +1,25 @@
+package com.ltts;
+
+public class ODIMatch extends Match {
+	public float calculateRunRate() {
+		int n;
+		float o,res;
+		n= getTarget()-getCurrentscore();
+		o=50-getCurrentover();
+		res=n/o;
+		return res;
+	}
+	public int calculateBalls()
+	{
+		int m;
+		m=(int)((50-getCurrentover())*6);
+		return m;
+	}
+	public void display(double reqRunrate,int balls)
+	{
+		System.out.println("Need " + (getTarget()-getCurrentscore()) +"runs in " +balls+"balls");
+		System.out.println("Required Runrate:"+reqRunrate);
+		
+	}
+	
+}
