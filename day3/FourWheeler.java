919f2483560c80340750ab223138e8db98eef1c1
tejasaigolla <78849267+tejasaigolla@users.noreply.github.com>
2021-03-01 09:20:15 +0530

    Create FourWheeler.java

---
+++ day3/FourWheeler.java
@@ -0,0 +1,30 @@
+package com.ltts;
+
+public class FourWheeler extends Vehicle{
+		private String audioSystem;
+		private int numberOfDoors;
+		public FourWheeler(String make, String vehicleNumber, String fuelType, int fuelCapacity, int cc,
+				String audioSystem, int numberOfDoors) {
+			super(make, vehicleNumber, fuelType, fuelCapacity, cc);
+			this.audioSystem = audioSystem;
+			this.numberOfDoors = numberOfDoors;
+		}
+		public String getAudioSystem() {
+			return audioSystem;
+		}
+		public void setAudioSystem(String audioSystem) {
+			this.audioSystem = audioSystem;
+		}
+		public int getNumberOfDoors() {
+			return numberOfDoors;
+		}
+		public void setNumberOfDoors(int numberOfDoors) {
+			this.numberOfDoors = numberOfDoors;
+		}
+		public void displayDetailInfo()		
+		{
+			System.out.println("___Detailed Information___");
+			System.out.println("Audio System:"+getAudioSystem());
+			System.out.println("Number of Doors:"+getNumberOfDoors());
+		}
+}
