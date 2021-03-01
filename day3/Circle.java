99e12f0c2662e14ae19ebafd2961f166c3e49ccd
tejasaigolla <78849267+tejasaigolla@users.noreply.github.com>
2021-03-01 09:23:13 +0530

    Create Circle.java

---
+++ day3/Circle.java
@@ -0,0 +1,11 @@
+package com.ltts;
+
+public class Circle extends Shape{
+	void calculateArea(int value)
+	{
+		double aoc;
+		aoc=3.14*value*value;
+		System.out.println("Area of Cicle is:"+aoc);
+	}
+
+}
